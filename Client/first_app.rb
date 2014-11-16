6337386a86034689a3cc8ce056a418c52eebedd8
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2014-11-16 01:20:10 +0100

    Login window

--- Client/first_app.rb
+++ Client/first_app.rb
@@ -1,25 +1,18 @@
-Shoes.app(width: 300, height: 400) {
-    stack(margin: 10) do
-        flow do
-            para 'Your ID'
-            @id_edit = edit_line
-            #@id_edit.move(left: 10)
+Shoes.app(width: 300, height: 120) {
+    stack do
+        flow margin: 10 do
+            stack width: '30%' do
+                para 'Your ID'
+                para 'Password'
+            end
+            stack width: '50%' do
+                @id_edit = edit_line
+                @pass_edit = edit_line secret: true
+
+                @login_button = button 'Login'
+                @login_button.click { exit }
+                @login_button.style margin_top: 10
+            end
         end
-        flow do
-            para 'Password'
-            @pass_edit = edit_line secret: true
-            @pass_edit.displace(left: 10)
-        end
-
-        #@edit_box = edit_box do |e|
-        #    @copy_box.text = @edit_box.text
-        #end
-
-        #@copy_box = para "What you type in the edit box goes here"
-
-        #@button = button("obfuscate") do
-        #    ob_bytes = @copy_box.text.each_byte.map { |b| b + 1 }
-        #    @copy_box.text = ob_bytes.map { |b| b.chr}.join
-        #end
     end
 }
