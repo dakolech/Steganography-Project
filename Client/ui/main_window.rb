96f8c041324ac6f7bb57df28c92a7d89deecee01
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2014-11-17 22:20:13 +0100

    Finish work on UI

--- Client/ui/main_window.rb
+++ Client/ui/main_window.rb
@@ -4,46 +4,50 @@ class MainWindow < Shoes
     url '/main',    :show
 
     def show
-        window height: 450, width: 600, resizable: false do
-            stack width: '70%', height: 400, scroll: true do
-                #border red, strokewidth: 1
-                20.times { inscription 'This is a messenger' }
+        window height: 435, width: 600, resizable: false do
+            stack width: '70%' do
+                stack height: 400, scroll: true do
+                    20.times { inscription 'This is a messenger' }
+
+                end
+                flow margin_top: 4 do
+                    edit_line width: '80%'
+                    button 'Send', width: '20%'
+                end
             end
             stack width: '30%' do
-                #border red, strokewidth: 1
-                friends = MainWindowHelper::load_friends
-                friends.each do |friend|
-                    flow margin_top: 2, margin_bottom: 2 do
-                        flow width: '80%' do
-                            leave do |f|
-                                f.clear { inscription friend[0] }
-                            end
-                            hover do |f|
-                                f.clear do
-                                    f.background rgb(180, 180, 180)
-                                    inscription friend[0]
+                stack height: 400, scroll: true do
+                    friends = MainWindowHelper::load_friends
+                    friends.each do |friend|
+                        flow margin_top: 2, margin_bottom: 2 do
+                            stack width: '80%' do
+                                leave do |f|
+                                    f.clear { inscription friend[0] }
                                 end
+                                hover do |f|
+                                    f.clear do
+                                        f.background rgb(180, 180, 180)
+                                        inscription friend[0]
+                                    end
+                                end
+                                inscription friend[0]
                             end
-                            inscription friend[0]
-                        end
-                        flow width: '20%' do
-                            leave { |f| f.clear }
-                            hover do |f|
-                                f.clear do
-                                    #image '../data/delete_red.png'
-                                    inscription 'Del', underline: 'single'
+                            stack width: '20%' do
+                                leave { |f| f.clear }
+                                hover do |f|
+                                    f.clear do
+                                        inscription 'Del', underline: 'single'
+                                    end
                                 end
                             end
                         end
                     end
                 end
-            end
-            flow width: '100%' do
-                #border red, strokewidth: 1
-                @actions = ['Send', 'Add new contact', 'Logout']
-                @actions.each do |action|
-                    stack width: "#{100/@actions.length}%", margin_left: 2, margin_right: 2 do
-                        button action, width: '100%' do
+                flow do
+                    @actions = ['Add new...', 'Logout']
+                    @actions.each do |action|
+                        button action, margin_top: 4,
+                                        width: "#{100/@actions.length}%" do
                             close
                         end
                     end
