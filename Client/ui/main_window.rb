6858773ef567771f64e5e8fe2ad878e87ef43e03
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2014-11-17 17:59:02 +0100

    End most of work with UI

--- Client/ui/main_window.rb
+++ Client/ui/main_window.rb
@@ -4,44 +4,48 @@ class MainWindow < Shoes
     url '/main',    :show
 
     def show
-        window height: 500 do
-            flow width: '100%' do
-                border red, strokewidth: 1
-                @actions = ['Logout', 'Add new contact']
-                @actions.each do |action|
-                    stack width: "#{100/@actions.length}%", margin_left: 2, margin_right: 2 do
-                        leave { |f| f.clear { para action, size: 10 } }
-                        hover do |f|
-                            f.clear do
-                                @hover_bg = f.background rgb(180, 180, 180)
-                                para action, size: 10
-                            end
-                        end
-                        click { |f| close }
-                        para action, size: 10
-                    end
-                end
-            end
-            stack width: '70%' do
-                border red, strokewidth: 1
-                para 'This is a messenger'
+        window height: 450, width: 600, resizable: false do
+            stack width: '70%', height: 400, scroll: true do
+                #border red, strokewidth: 1
+                20.times { inscription 'This is a messenger' }
             end
             stack width: '30%' do
-                border red, strokewidth: 1
+                #border red, strokewidth: 1
                 friends = MainWindowHelper::load_friends
                 friends.each do |friend|
                     flow margin_top: 2, margin_bottom: 2 do
-                        leave do |f|
-                            f.clear { para friend[0], size: 10 }
+                        flow width: '80%' do
+                            leave do |f|
+                                f.clear { inscription friend[0] }
+                            end
+                            hover do |f|
+                                f.clear do
+                                    f.background rgb(180, 180, 180)
+                                    inscription friend[0]
+                                end
+                            end
+                            inscription friend[0]
                         end
-                        hover do |f|
-                            f.clear do
-                                @hover_bg = f.background rgb(180, 180, 180)
-                                para friend[0], size: 10
-                                para 'Del', size: 10
+                        flow width: '20%' do
+                            leave { |f| f.clear }
+                            hover do |f|
+                                f.clear do
+                                    #image '../data/delete_red.png'
+                                    inscription 'Del', underline: 'single'
+                                end
                             end
                         end
-                        para friend[0], size: 10
+                    end
+                end
+            end
+            flow width: '100%' do
+                #border red, strokewidth: 1
+                @actions = ['Send', 'Add new contact', 'Logout']
+                @actions.each do |action|
+                    stack width: "#{100/@actions.length}%", margin_left: 2, margin_right: 2 do
+                        button action, width: '100%' do
+                            close
+                        end
                     end
                 end
             end
