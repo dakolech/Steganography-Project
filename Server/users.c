d436be338787708f94c4b5e71a99c00fa7f8116a
Daniel Koza <dakolech@gmail.com>
2014-12-12 21:32:49 +0100

    small fixes

--- Server/users.c
+++ Server/users.c
@@ -15,16 +15,19 @@ int checkUserLoginPass(char * login, char * pass) {
 		exit(EXIT_FAILURE);
 	}
 
-	//int count = 0;
+	int count = 0;
 	int searchPass = 0;
 	while( fgets(line, 25,fp) ){
+		count++;
 		len = strlen(line);
 		if( line[len-1] == '\n' )
 			line[len-1] = 0;
-		if (searchPass == 1 && strcmp(pass,line) == 0)
+		if (searchPass == 1 && strcmp(pass,line) == 0 && count%2 == 0)
 			return 0;
+		else
+			searchPass = 0;
 
-		if (strcmp(login,line) == 0) 
+		if (strcmp(login,line) == 0 && count%2) 
 			searchPass = 1;
 	}
 
